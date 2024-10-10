626eaa087ad4d868276f1b263421ca4030d084a9
andreansyah05 <permanaandreansyah78@gmail.com>
2024-10-10 22:06:15 +0700

    add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import {QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import axios from 'axios'
+import axios from "axios";
 
 const queryClient = new QueryClient()
 axios.defaults.baseURL= process.env.NEXT_PUBLIC_CMS_CDN_URL
